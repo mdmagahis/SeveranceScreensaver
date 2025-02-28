66387fef3bb19647766e27f980ec651ceca413ba
Mark Magahis <mark.magahis@gmail.com>
2025-02-28 13:00:05 -0500

    Initial Commit

---
+++ SeveranceScreensaver/SeveranceScreensaverView.h
@@ -0,0 +1,12 @@
+//
+//  SeveranceScreensaverView.h
+//  SeveranceScreensaver
+//
+//  Created by Mark Magahis on 2025.02.28.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface SeveranceScreensaverView : ScreenSaverView
+
+@end
