32c4ba4a400a7955e8a3e3ba760c0c25d9ee3b96
Mark Magahis <mark.magahis@gmail.com>
2025-04-10 21:03:03 -0400

    🎨 improve structure to follow Apple's Screen Saver conventions
     🔥 remove main.swift, unused
     👾 clean up white space

--- SeveranceScreensaver/SeveranceScreensaverView.swift
+++ SeveranceScreensaver/SeveranceScreensaverView.swift
@@ -14,24 +14,35 @@ class SeveranceScreensaverView: ScreenSaverView {
 
     override init?(frame: NSRect, isPreview: Bool) {
         super.init(frame: frame, isPreview: isPreview)
-        setupText()
     }
 
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+    }
+
+    override func startAnimation() {
+        super.startAnimation()
         setupText()
+    }
+
+    override func stopAnimation() {
+        super.stopAnimation()
+    }
+
+    override func draw(_ rect: NSRect) {
+        NSColor.black.setFill()
+        rect.fill()
     }
 
     private func setupText() {
         self.wantsLayer = true
         self.layer?.backgroundColor = NSColor.black.cgColor
-        
+
         let fontSize: CGFloat = bounds.height * 0.1
         let defaultFontSpec: NSFont = NSFont.systemFont(ofSize: fontSize, weight: .bold)
         let fontSpec = NSFont(name: "Michroma", size: fontSize)
         let atributes: [NSAttributedString.Key: Any] = [.font: fontSpec ?? defaultFontSpec]
 
-        
         let startX = bounds.width
         let startY = bounds.midY - fontSize / 2
         let textWidth: CGFloat = CGFloat( text.size(withAttributes: atributes).width )
@@ -45,12 +56,12 @@ class SeveranceScreensaverView: ScreenSaverView {
             charLayer.foregroundColor = NSColor.white.cgColor
             charLayer.alignmentMode = .center
             charLayer.contentsScale = NSScreen.main?.backingScaleFactor ?? 2.0
-            
+
             let charSpace: CGSize = String(char).size(withAttributes: atributes)
             print("char:", char, "| width:", charSpace.width, "| pos:", charPosition)
             let xPosition = charPosition
             charLayer.frame = CGRect(x: xPosition, y: startY, width: CGFloat(charSpace.width), height: CGFloat(charSpace.height))
-            
+
             self.layer?.addSublayer(charLayer)
             characterLayers.append(charLayer)
             charPosition += CGFloat(charSpace.width)
@@ -68,40 +79,38 @@ class SeveranceScreensaverView: ScreenSaverView {
             configSpringAnimation(layer: moveToCenter, index: index)
             moveToCenter.fromValue = charLayer.position.x
             moveToCenter.toValue = centerX - (textWidth / 2) + charLayer.position.x - bounds.width
-            
+
             charLayer.add(moveToCenter, forKey: "moveToCenter")
             totalDuration += moveToCenter.settlingDuration
         }
-        
+
         let moveOutDelay = totalDuration / 5 // Time before moving out
         DispatchQueue.main.asyncAfter(deadline: .now() + moveOutDelay) {
             self.animateExit(centerX: centerX, textWidth: textWidth)
         }
     }
 
-    func animateExit(centerX: CGFloat, textWidth: CGFloat) {
+    private func animateExit(centerX: CGFloat, textWidth: CGFloat) {
         var totalDuration: CFTimeInterval = 0
 
         // Animate moving out to the left
         for ( index, charLayer ) in characterLayers.enumerated() {
-            
-            
             let moveOut = CASpringAnimation(keyPath: "position.x")
             configSpringAnimation(layer: moveOut, index: index)
             moveOut.fromValue = centerX - (textWidth / 2) + charLayer.position.x - bounds.width
             moveOut.toValue = -(charLayer.position.x)
-            
+
             charLayer.add(moveOut, forKey: "moveOut")
             totalDuration += moveOut.settlingDuration
         }
-        
+
         // Restart animation after exit
         DispatchQueue.main.asyncAfter(deadline: .now() + totalDuration / 7) {
             self.animateIn(textWidth: textWidth)
         }
     }
-    
-    func configSpringAnimation(layer: CASpringAnimation, index: Int) {
+
+    private func configSpringAnimation(layer: CASpringAnimation, index: Int) {
         let delay = Double(index) * 0.2 // Stagger animation for each character
         layer.damping = 1500
         layer.stiffness = 100
@@ -112,5 +121,4 @@ class SeveranceScreensaverView: ScreenSaverView {
         layer.fillMode = .forwards
         layer.beginTime = CACurrentMediaTime() + delay
     }
-
 }
